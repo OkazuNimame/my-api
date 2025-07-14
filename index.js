ceb2f7c183c96ff252c0e69088d2d5eb67b4b96d
OkazuNimame <yaese13sora@gmail.com>
2025-07-14 20:15:28 +0900

    sss

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('🌐 Railway Node.js サーバーが動いてるよ！');
+});
+// POST /text エンドポイント
+app.post('/text', (req, res) => {
+  const { message } = req.body;
+  console.log('受け取ったメッセージ:', message);
+
+  res.json({
+    response: `受け取ったよ: ${message}`
+  });
 });
 
 const PORT = process.env.PORT || 3000;
