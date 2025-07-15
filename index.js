ab240fc0f37076ef5e40a59cf1fb2fed24be2ca4
OkazuNimame <yaese13sora@gmail.com>
2025-07-15 20:14:08 +0900

    tt

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const multer = require('multer');
 const cors = require('cors');
-const cloudinary = require('./cloudinary');
-const fs = require('fs');
-
 const app = express();
 app.use(cors());
 
-const upload = multer({ dest: 'uploads/' }); // 一時保存フォルダ
 
 app.post('/flutter',(req,res) => {
   const {message} = req.body;
@@ -17,21 +12,3 @@ app.post('/flutter',(req,res) => {
     response:`受け取ったよ${message}`
   });
 });
-
-app.post('/upload', upload.single('image'), async (req, res) => {
-  try {
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      folder: 'flutter_uploads', // Cloudinary内のフォルダ
-    });
-
-    fs.unlinkSync(req.file.path); // 一時ファイル削除（不要なら省略OK）
-
-    res.json({ url: result.secure_url }); // Flutterに画像URL返す
-  } catch (err) {
-    console.error('Upload error:', err);
-    res.status(500).json({ error: 'Upload failed' });
-  }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
