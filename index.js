47043bcaf427189ef37edb1b53e4df8a2e589d95
OkazuNimame <yaese13sora@gmail.com>
2025-07-15 20:26:35 +0900

    っっっ

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
 
-app.get('/',(req,res) => {
+app.use(cors());
+app.use(express.json()); // ★←これが重要！
+
+app.get('/', (req, res) => {
   res.send('Hello!!');
 });
 
-
-app.post('/flutter',(req,res) => {
-  const {message} = req.body;
-  console.log('受け取ったメッセージ',message);
+app.post('/flutter', (req, res) => {
+  const { message } = req.body;
+  console.log('受け取ったメッセージ', message);
 
   res.json({
-    response:`受け取ったよ${message}`
+    response: `受け取ったよ ${message}`,
   });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
